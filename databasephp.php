066d519c8d657c6248a829ff188efdc6fa582892
violeteyes24 <gmzach024@gmail.com>
2023-11-07 11:58:51 +0800

    change variables for another set of data employees

--- databasephp.php
+++ databasephp.php
@@ -2,36 +2,36 @@
 <body>
 <!-- showing off details, $POST - associative array of variables passed to current script via HTTP POST method -->
 Welcome 
-<?php echo $_POST["name"]; ?>
+<?php echo $_POST["emp_no"]; ?>
 <br>
 Your email address is:
-<?php echo $_POST["email"]; ?>
+<?php echo $_POST["birth_date"]; ?>
 <br>
 Your age is:
-<?php echo $_POST["age"]; ?>
+<?php echo $_POST["first_name"]; ?>
 <br>
 You live in:
-<?php echo $_POST["Country"]; ?>
+<?php echo $_POST["last_name"]; ?>
 <br>
 Your gender is:
 <?php echo $_POST["gender"]; ?>
 <br>
 Your interest are:
-<?php echo $_POST["interests"]; ?>
+<?php echo $_POST["hire_date"]; ?>
 <br>
 
 <?php
 $servername = "localhost"; // default for xampp
 $username = "root"; // default for xampp
 $password = ""; // default for xampp
-$database = "zlegaria"; // dbname
+$database = "zlegariaa"; // dbname
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$age = $_POST['age'];
-$country = $_POST['Country'];
+$empno = $_POST['emp_no'];
+$bdate = $_POST['birth_date'];
+$fname = $_POST['first_name'];
+$lname = $_POST['last_name'];
 $gender = $_POST['gender'];
-$interests = $_POST['interests'];
+$hdate = $_POST['hire_date'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -43,12 +43,12 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 if (isset($_POST['submit_button'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $age = $_POST['age'];
-    $country = $_POST['Country'];
+    $empno = $_POST['emp_no'];
+    $bdate = $_POST['birth_date'];
+    $fname = $_POST['first_name'];
+    $lname = $_POST['last_name'];
     $gender = $_POST['gender'];
-    $interests = $_POST['interests'];
+    $hdate = $_POST['hire_date'];
 }
 
 try {
@@ -56,7 +56,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = "INSERT INTO `users` (name, email, age, Country, gender, interests)
-            VALUES ('$name','$email','$age','$country','$gender','$interests')";
+            VALUES ('$empno','$bdate','$fname','$lname','$gender','$hdate')";
 
     $conn->exec($sql);
     echo "New record inserted successfully";
